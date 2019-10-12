a4c424abf2c0484fe75197e3320a874819c53f62
Charlie Oster <oster.charlie@yahoo.com>
2019-10-12 00:21:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
+def oxford_comm(array) #[kiwi,durian,starfruit]
+ if array.length == 1
+   return "#{array[0]}" #{kiwi","durian","starfruit"}
+ elsif array.length == 2
+   return array.join(" and ") #"kiwi"and"durian"and "starfruit"
+ elsif array.length >= 3
+   array[-1] = "and #{array[-1]}"
+   return array.join(", ") # "kiwi","durian", "starfruit"... "a","b","b"
+end
 end
